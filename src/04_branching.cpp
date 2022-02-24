47c5a97ec3bb242272fd125eccd94e96df019830
Ramil Safin <RamilSafNab1996@gmail.com>
2022-02-24 10:01:28 +0300

    Minor refactor

--- src/04_branching.cpp
+++ src/04_branching.cpp
@@ -1,20 +1,42 @@
-// Исходный код программы с операторами ветвления С++.
+// Исходный код программы на С++.
 
 int main() {
 
   int magick_number = 215;
 
-  // 1. if-else
+  // 1. if
   if (magick_number > 200) {
     // ветка 1
     magick_number = 200;
-  } else if (magick_number > 150 && magick_number < 180) {
+  }
+
+  // 2. if-else
+  if (magick_number == 200) {
+    // ветка 1
+    magick_number = 210;
+  } else {
     // ветка 2
-    magick_number = 150;
+    magick_number = 310;
+  }
+
+  // 3. if-else-if-else
+  if (magick_number == 210) {
+    // ветка 1
+    magick_number = 1;
+  } else if (magick_number == 310) {
+    // ветка 2
+    magick_number = 2;
   } else {
     // ветка 3
-    magick_number = 0;
+    magick_number = 3;
   }
+
+  // еще есть switch-case ... он нам особо не понадобится
 
   return 0;
 }
+
+/*
+ * Задания:
+ * 1. Как проверять более сложные (составные) условия?
+ */
